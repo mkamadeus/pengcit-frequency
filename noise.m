48f4f57b255412dba1a2d23daf483774802f7558
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2022-02-28 13:27:47 +0700

    feat: noise driver modified

--- noise.m
+++ noise.m
@@ -1,17 +1,23 @@
-[img, m] = truecolorload("./images/boat.bmp");
+img = truecolorload("./images/boat.bmp");
+img = img(:, :, 1);
 
 subplot(3,2,1);
-imshow(img);title("original");
+imshow(img, []);title("original");
 
 subplot(3,2,2);
 [fimg, shiftedfimg] = spatial2freq(img);
-imshow(shiftedfimg, []);title("og spectrum");
+% disp(fimg);
+% disp(shiftedfimg);
+imshow(im2double(shiftedfimg), []);title("og spectrum");
 
 subplot(3,2,3);
-noised = makeperiodicnoise(img, 1/5, 1/5);
-imshow(noised);title("noised og");
+noised = makeperiodicnoise(img, 1/10, 1/10);
+imshow(noised, []);title("noised og");
 
 subplot(3,2,4);
-[fnoised, shiftedfnoised] = spatial2freq(fnoised);
-disp(shiftedfnoised);
-imshow(shiftedfnoised, []);title("noised og spectrum");
+[fnoised, shiftedfnoised] = spatial2freq(noised);
+s = size(shiftedfnoised)
+imshow(im2double(shiftedfnoised), []);title("noised og spectrum");
+
+disp(im2double(shiftedfnoised(s(1)/2 - 50:s(1)/2, s(2)/2-50:s(2)/2)));
+disp(sort(shiftedfnoised(:)));
