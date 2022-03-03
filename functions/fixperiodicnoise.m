590e1363e7e71ee6fa44c6e4347d4f3c37ef3192
ozer0532 <ozer0532@gmail.com>
2022-03-03 14:58:12 +0700

    Integrate notch filter

--- functions/fixperiodicnoise.m
+++ functions/fixperiodicnoise.m
@@ -1,4 +1,18 @@
-function res = fixperiodicnoise(img, ranges)
-    fimg = spatial2freq(img);
+function [res, F] = fixperiodicnoise(I, filters)
+    [F, ~] = spatial2freq(I);
+    F = fftshift(F);
     
+    % Apply notch filters
+    filter_count = size(filters, 1);
+    for i = 1:filter_count
+        x1 = filters(i, 1);
+        y1 = filters(i, 2);
+        x2 = filters(i, 3);
+        y2 = filters(i, 4);
+
+        F(x1:x2, y1:y2) = 0;
+    end
+    
+    % Freq => spatial
+    res = freq2spatial(ifftshift(F));
 end
